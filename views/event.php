7a31df899d4411bfa7116328ea2aca847679c380
Willem Van der Merwe <will.vdmerwe@sliderstudio.co.uk>
2012-02-17 16:14:23 +0000

    added callAPI.php in includes for AJAX calls to API - works for GET - authorisation for POST|PUT|DELETE requires much testing

--- views/event.php
+++ views/event.php
@@ -2,4 +2,23 @@
 
 EDIT OR CREATE IF NO EVENT IS SET
 
-<?php var_dump($collections);?>
+<?php //var_dump($collections);?>
+
+<script type="text/javascript">
+$.ajax("includes/callAPI?action=event/get&id=3&include_card_count=1", {
+    success: function(data){
+        try {
+            var data = $.parseJSON(data);
+            //
+            // DO STUFF HERE. DATA CONTAINS THE OBJECT REQUESTED
+            //
+        } catch (err) {
+            //
+            // ERROR. data SHOULD CONTAIN DESCRIPTION OF WHAT WENT WRONG
+            //
+            // displayAlertMessage(data);
+        }
+    },
+    error: function(data){ console.log(data); }
+});
+</script>
