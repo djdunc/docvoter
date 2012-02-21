c21eef06f4cb7e074534bfe68c0bbf382f45a25d
malicias <malicias@gmail.com>
2012-02-21 12:04:31 +0000

    deck saving to api

--- views/deck.php
+++ views/deck.php
@@ -26,17 +26,17 @@
 		    <span class="message"></span>
 			<div class="content no-cap">
 				<!-- Any form you want to use this custom styling with must have the class "styled" -->
-				<form id="event" class="styled">
+				<form id="deck" class="styled">
 					<fieldset>
 						<!-- Text Field -->
 						<label class="align-left">
 							<span>Deck name<strong class="red">*</strong></span>
-							<input class="textbox l required editable" name="name" id="name" type="text" value="<?php if(isset($edit_event->name)){echo($edit_event->name);}?>" />
+							<input class="textbox l required editable" name="name" id="name" type="text" value="<?php if(isset($edit_deck->name)){echo($edit_deck->name);}?>" />
 						</label>
 						<!-- Text Area -->
 						<label class="align-left" for="textArea">
 							<span>Deck description</span>
-							<textarea class="textarea l editable" name="description" id="description" rows="2" cols="1"><?php if(isset($edit_event)){echo($edit_event->description);}?></textarea>
+							<textarea class="textarea l editable" name="description" id="description" rows="2" cols="1"><?php if(isset($edit_deck)){echo($edit_deck->description);}?></textarea>
 						</label>
 						<!-- Buttons -->
 						<div class="non-label-section">
@@ -69,7 +69,7 @@
     			</div>
 		</div>
 	</div>
-	<?php if (isset($event_cards)){?>
+	<?php if (isset($deck_cards)){?>
 	<div class="grid_4">
 	    <div class="panel">	
 	        <h2 class="cap">Event cards</h2>
@@ -77,7 +77,7 @@
 			<div class="content gallery">
 				<div class="gallery-wrap">
 					<div class="gallery-pager">
-						<?php foreach (array_reverse($event_cards) as $card) { ?>
+						<?php foreach (array_reverse($deck_cards) as $card) { ?>
 						<!-- GALLERY ITEM -->
 						<div class="gallery-item">
 							<a class="clue" title="<?php echo $card->name; ?>" href="index.php?do=view&card_id=<?php echo $card->id ?>"><img src="<?php if (isset($card->card_front)){ echo (UPLOADS_URL."fronts/".$card->card_front."_t.jpg");}?>" alt="" /></a>
@@ -115,3 +115,73 @@
 	<?php }?>
  </div>
 </div>
+<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.0/themes/base/jquery-ui.css" type="text/css" media="all" />
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
+<script type="text/javascript" src="http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.min.js"></script>
+<?php if(!isset($edit_deck_id)){$edit_deck_id=0;} ?>
+<script type="text/javascript">//<![CDATA[
+    $(document).ready(function() {
+    var formChanged = false;
+    var baseurl = "<?php echo BASE_URL; ?>";
+    var edit_deck = <?php echo($edit_deck_id);?>;
+    var owner = <?php echo $_SESSION['user']->id;?>;
+    //if adding deck edit_deck == 0
+    if (edit_deck != 0){
+        var action = 'controller=api&action=deck/put&id='+edit_deck;
+    } else{
+        var action = 'controller=api&action=deck/post&owner='+owner;
+    }
+    
+    $("#deck").validate({
+        rules: { name: "required"},
+        messages: { name: "Deck name is required"}
+    });
+    
+    $('#deck input.editable, #deck textarea.editable').each(function (i) {
+         $(this).data('initial_value', $(this).val());
+    });
+
+    $('#deck input.editable, #deck textarea.editable').keyup(function() {
+         if ($(this).val() != $(this).data('initial_value')) {
+              handleFormChanged();
+         }
+    });
+    
+    //if something is edited, show save button, and display alert on page leave
+
+    $('#deck .editable').bind('change paste', function() {
+         handleFormChanged();
+    });
+    
+    //bind save button
+    $("#save").click(function() {
+      if($("#deck").valid()){
+          $(window).unbind("beforeunload");
+         //alert(action+'&'+$("#deck").serialize());    
+          $.post('includes/callAPI.php', action+'&'+$("#deck").serialize(), function(data) {
+                  var saved_deck = eval(jQuery.parseJSON(data));
+                  if (saved_deck.id){
+                      displayAlertMessage("Deck saved!");
+                  } else{
+                       displayAlertMessage(data);
+                  }
+         }).error(function() { alert("There was an error saving your deck, please try again later."); })
+      }
+      return false;
+    });
+});
+
+   function handleFormChanged() {
+        $(window).bind('beforeunload', confirmNavigation);
+        $('#save').show();
+        $('#fakesave').hide();
+        formChanged = true;
+   }
+   function confirmNavigation() {
+        if (formChanged) {
+             return ('One or more forms on this page have changed. Your changes will be lost!');
+        } else {
+             return true;
+        }
+   }
+//]]></script>
