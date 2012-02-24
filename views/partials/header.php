4ac468c1d3d2340573208085b6bcdff2f1ceea55
malicias <malicias@gmail.com>
2012-02-24 17:41:26 +0000

    Category mapping issues

--- views/partials/header.php
+++ views/partials/header.php
@@ -97,13 +97,13 @@
 		    if(!in_array($page, $admin_pages) && isset($data['event'])) {?>
 		    <h1 class="grid_2">
 		        <a href="<?php echo BASE_URL;?>index.php?event=<?php echo $data['event']->id; ?>">
-		            <span class="org"><?php if(isset($data['event_org']))echo $data['event_org']->name?>&nbsp;</span>
+		            <span class="org"><?php if(isset($data['event_org']))echo $data['event_org']->name;?>&nbsp;</span>
 			        <?php echo $data['event']->name; ?>
 			    </a>
 		    </h1>
 	        <div class="grid_2">
 	            <ul id="main-navigation">
-                	<li><a href="index.php?event=<?php echo $data['event']->id; ?>"<?php if($page == 'about'){ echo(" class=\"active\"");} ?>>about</a></li><li><a href="index.php?do=vote&event=<?php echo $data['event']->id; ?>"<?php if($page == 'vote'){ echo(" class=\"active\"");} ?>>vote</a></li><li class="last"><a href="index.php?do=vote&event=<?php echo $data['event']->id; ?>"<?php if($page == 'results'){ echo(" class=\"active\"");} ?>>results</a></li>
+                	<li><a href="index.php?event=<?php echo $data['event']->id; ?>"<?php if($page == 'about'){ echo(" class=\"active\"");} ?>>about</a></li><?php if(is('user')){?><li><a href="index.php?do=vote&event=<?php echo $data['event']->id; ?>"<?php if($page == 'vote'){ echo(" class=\"active\"");} ?>>vote</a></li><?php }?><li class="last"><a href="index.php?do=vote&event=<?php echo $data['event']->id; ?>"<?php if($page == 'results'){ echo(" class=\"active\"");} ?>>results</a></li>
                 </ul>
 		    </div>
 		<?php //admin nav
