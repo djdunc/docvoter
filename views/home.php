dd708e86a32d5a3cf3a8bacfd7460a919fb33d75
malicias <malicias@gmail.com>
2012-02-26 19:54:12 +0000

    event listing includes org & votes (only in look, not yet implemented)

--- views/home.php
+++ views/home.php
@@ -13,16 +13,30 @@
     </div>
 </div>
 </div>
+<div class="grey">
 <div class="container_4">
 	<div id="page-heading" class="clearfix">
 	    <div class="grid-wrap">
     		<div class="grid_4">
-    		       <ul>
-    		           <?php foreach ($data['events'] as $event) { ?>
-    		          <li><a href="<?php echo (BASE_URL.'index.php?event='.$event->id);?>"><?php echo($event->name);?></a></li>
+    		       <ul id="event-gallery">
+    		           <?php foreach ($events as $event){?>
+    		          <li><a href="<?php echo (BASE_URL.'index.php?event='.$event->id);?>"> 
+    		              <p class="drivers">
+    		                  
+    		                  <?php if($event->end!=0 && $event->end < time()) { ?>
+    		                  <span class="social">viral/bacterial pandemic</span> <span class="technological">full scale nuclear war</span> <span class="environmental">rapid rise in life expentancy</span> <span class="economic">gulf stream shuts down</span> <span class="political">lifelong working</span>
+    		                  <?php }else {?>
+    		                      <span class="">Have your say!</span>
+    		                  <?php }?>
+    		                  </p>
+    		              <p class="footer"><span class="event"><?php echo($event->name);?></span>
+    		              <span class="org">by org here</span> <span class="votes">NN votes</span></p>
+    		              </a>
+    		          </li>
     		          <?php }?>
     		       </ul>
     		</div>
 	    </div>
     </div>
+</div>
 </div>
